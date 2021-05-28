805cdae548e37be71f693fcfc598ac42e15001f4
AOhnemus <49699686+AOhnemus@users.noreply.github.com>
2021-05-28 11:58:33 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ using namespace std;
  * - Crashes if you attempt to attack an empty cell. Implement error handling.
  * - Non-technical issue. I'm going to need a second file for all attacks, which needs to be joined with the
  * existing units.csv stuff, because each creature has different kinds of, and numbers of, abilities.
- * CSV might actually be a terrible format. I'm beginning to wonder how the hell we make this work.
+ * CSV might actually be a terrible format. I'm beginning to wonder how the we make this work.
  * How am I going to design this? Gonna put some thought into it.
  */
 
